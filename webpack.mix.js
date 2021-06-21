40e7b4a5765393f6cfa73215e053dcd13a8b0aa6
Antoine Chassagne <antoine.chassagne@me.com>
2021-06-21 14:55:58 +0200

    Switch from CSS to SCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,3 @@
-const mix = require('laravel-mix')
+const mix = require('laravel-mix');
 
-mix
-  .css('src/css/index.css', 'public/assets/css/index.css')
-  .js('src/js/index.js', 'public/assets/js/index.js')
+mix.sass('src/scss/index.scss', 'public/assets/css/index.css').js('src/js/index.js', 'public/assets/js/index.js');
