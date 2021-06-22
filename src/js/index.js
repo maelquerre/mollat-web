45065ccf33007e949c8c0504336a5bd3f800166c
Antoine Chassagne <antoine.chassagne@me.com>
2021-06-22 15:17:26 +0200

    Add banner section

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,26 @@
 import Alpine from 'alpinejs';
+import dayjs from 'dayjs';
 
 window.Alpine = Alpine;
+
+/* Banner */
+window.setupBannerData = function () {
+  const today = dayjs();
+  const christmas = dayjs()
+    .set('day', 24)
+    .set('month', 11)
+    .set('year', today.year())
+    .set('hours', 23)
+    .set('minutes', 59)
+    .set('seconds', 59);
+  let delta = christmas.diff(today) / 1000;
+  const daysLeft = Math.floor(delta / 86400);
+  delta -= daysLeft * 86400;
+  const hoursLeft = Math.floor(delta / 3600) % 24;
+  delta -= hoursLeft * 3600;
+  const minutesLeft = Math.floor(delta / 60) % 60;
+  delta -= minutesLeft * 60;
+  return { daysLeft, hoursLeft, minutesLeft };
+};
 
 Alpine.start();
