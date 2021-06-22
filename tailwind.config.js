f815a57e90bad920de3fedff20db391a78c894e8
Maël Querré <mael.querre@gmail.com>
2021-06-22 14:30:12 +0200

    Update colors structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      gray: '#595959',
-      darkGray: '#282828',
-      lightGray: '#f2f2f2',
-      blue: '#001689',
-      lightBlue: '#3a5fbf',
+      gray: {
+        DEFAULT: '#595959',
+        dark: '#282828',
+        light: '#f2f2f2',
+      },
+      blue: {
+        DEFAULT: '#001689',
+        light: '#3a5fbf',
+      },
       beige: '#ffeecf',
     },
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
-      display: ['Oswald', 'sans-serif']
+      display: ['Oswald', 'sans-serif'],
     },
     extend: {},
   },
