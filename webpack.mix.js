a6e13ead263422f8cbcc5e4d8f6ca5c22d2c6bef
Maël Querré <mael.querre@gmail.com>
2021-06-22 13:57:10 +0200

    Update css structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,8 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 mix
-  .sass('src/scss/index.scss', 'public/css/index.css')
+  .js('src/js/index.js', 'public/js/index.js')
+  .css('src/css/index.css', 'public/css/index.css')
   .options({
     processCssUrls: false,
   })
-  .js('src/js/index.js', 'public/js/index.js')
-  .copy('src/images/', 'public/images/', false);
