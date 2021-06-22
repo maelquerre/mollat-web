34e7d166c293b2e8f33d9e98d354db76e3b5192a
Maël Querré <mael.querre@gmail.com>
2021-06-22 14:22:32 +0200

    Add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
+      gray: '#595959',
       darkGray: '#282828',
-      gray: '#f2f2f2',
+      lightGray: '#f2f2f2',
       blue: '#001689',
+      lightBlue: '#3a5fbf',
+      beige: '#ffeecf',
     },
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
