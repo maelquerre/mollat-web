06cdf86f66104aeb2c5ed08ab3372b635be521e2
Maël Querré <mael.querre@gmail.com>
2021-06-22 15:12:51 +0200

    Add containers sizes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -17,6 +19,16 @@ module.exports = {
       },
       beige: '#ffeecf',
     },
+    container: {
+      center: true,
+      padding: '1rem',
+      sizes: {
+        sm: '620px',
+        md: '750px',
+        lg: '1000px',
+        '2xl': '1500px',
+      },
+    },
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
       display: ['Oswald', 'sans-serif'],
@@ -26,5 +38,29 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    plugin(function ({ addComponents, config }) {
+      const containersSizes = {}
+
+      Object.entries(config('theme.container.sizes')).forEach(([breakpoint, size]) => (
+        containersSizes[`@screen ${breakpoint}`] = {
+          maxWidth: size,
+        }
+      ))
+
+      addComponents({
+        '.container': {
+          width: '100%',
+          marginLeft: config('theme.container.center') ? 'auto' : '',
+          marginRight: config('theme.container.center') ? 'auto' : '',
+          paddingLeft: config('theme.container.padding'),
+          paddingRight: config('theme.container.padding'),
+          ...containersSizes,
+        }
+      })
+    })
+  ],
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix')
+const path = require('path')
 
 mix
   .js('src/js/index.js', 'public/js/index.js')
