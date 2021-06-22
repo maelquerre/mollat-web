d3eb1865e377c9d1f769b0f596de2d0fe0183326
Maël Querré <mael.querre@gmail.com>
2021-06-22 14:17:34 +0200

    Add fonts import

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      darkGray: '#282828',
+      gray: '#f2f2f2',
+      blue: '#001689',
+    },
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
+      display: ['Oswald', 'sans-serif']
     },
     extend: {},
   },
